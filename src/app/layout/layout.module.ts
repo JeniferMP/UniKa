2c06555de2122e44f61ad838db59bfe46dbdc0a7
NayelyReyes09 <69448532+NayelyReyes09@users.noreply.github.com>
2022-05-03 10:42:13 -0500

    login01

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,6 +5,8 @@ import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { UsuarioComponent } from './usuario/usuario.component';
 import { DahsboardComponent } from './dahsboard/dahsboard.component';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,7 +17,12 @@ import { DahsboardComponent } from './dahsboard/dahsboard.component';
   ],
   imports: [
     CommonModule,
-    LayoutRoutingModule
-  ]
+    LayoutRoutingModule,
+    SharedModule,
+    FormsModule
+  ],exports:[
+    SharedModule,
+    DahsboardComponent
+  ],
 })
 export class LayoutModule { }
