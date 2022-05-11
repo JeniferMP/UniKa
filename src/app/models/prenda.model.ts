9d92a04415e75ec00e97f387ec3b327fcf82bbaa
Junior Mendez <72240716+Junior-mendez@users.noreply.github.com>
2022-05-11 12:13:28 -0500

    Listado de Prendas.

--- src/app/models/prenda.model.ts
+++ src/app/models/prenda.model.ts
@@ -1,10 +1,11 @@
 export class Prenda {
     PREN_ID: number;
-    PREN_NOMBRE: string;
     PREN_CODIGO: string;
+    PREN_NOMBRE: string;
     PREN_MARCA: string;
     PREN_PRECIO: string;
     PREN_IMAGEN: string;
     PREN_ESTADO: string;
     CAT_ID: number;
+    CAR_NOMBRE: string;
 }
